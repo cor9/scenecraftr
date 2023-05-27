1172633a32b9255118115611f564c41061c0e482
cor9 <35475851+cor9@users.noreply.github.com>
2023-05-27 00:28:54 -0700

    Add files via upload

--- scene.js
+++ scene.js
@@ -1,12 +1,22 @@
-module.exports = (req, res) => {
-  const memberId = req.query.memberId;
-  
-  fetch('https://api.airtable.com/v0/app1IfFIhWJaLxjVW/user_scenes?filterByFormula={memberID}="'+memberId+'"', {
-      headers: {
-        'Authorization': 'Bearer keyfQ87Zzr8iuZajP'
-      }
-    })
-    .then(response => response.json())
-    .then(data => res.json(data.records))
-    .catch(error => res.json({ error: 'An error occurred' }));
-}
+
+
+const Airtable = require('airtable');
+
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+
+const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+
+module.exports = async (req, res) => {
+    const memberId = req.query.memberId;
+    
+    // Get records from Airtable base
+    let records = await base('User_Scenes').select({
+        filterByFormula: `{memberID} = "${memberId}"`
+    }).firstPage();
+    
+    res.json(records.map(record => record.fields));
+};
+
